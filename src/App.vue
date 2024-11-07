9d5f715a3a6f4db75ce85d7e29ae94ab67319c7c
unknown <dipesh.p.bhatia@gmail.com>
2024-11-07 13:46:28 +0530

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div>
+  <app-header />
+  <router-view/>
+  <br>
+  <AirportComponent/>
+  <br>
+  <app-footer/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from '@/components/AppHeader.vue'
+import AirportComponent from './components/AirportComponent.vue';
+
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    AppHeader,
+    AirportComponent,
+    AppFooter
   }
 }
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+</script>
---
+++ src/components/AirportComponent.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper">
+    <div v-for="airport in airports" :key="airport.abbreviation" class="airport">  
+        <p>{{ airport.abbreviation }}</p>
+        <p>{{ airport.name }}</p>
+        <p>{{ airport.city }}</p>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import devairports from '@/../public/airports.development.mock'
+import stageairports  from '@/../public/airports.staging.mock'
+
+export default {
+  
+  computed: {
+    airports() {
+      // Check the environment variable and select the appropriate data
+      console.log("Current enviornment" , process.env.VUE_APP_ENV);
+      if (process.env.VUE_APP_ENV === 'development') {
+        return devairports;
+      } else if (process.env.VUE_APP_ENV === 'staging')
+        return stageairports;
+       else {
+      return null
+    
+    }
+    }
+  }
+}
+
+</script>
+
+<style>
+
+
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1rem;
+  max-width: 960px;
+  margin: 0 auto;
+}
+
+.airport {
+  border: 3px solid;
+  border-radius: .5rem;
+  padding: 1rem;
+}
+
+.airport p:first-child {
+  font-weight: bold;
+  font-size: 2.5rem;
+  margin: 1rem 0;
+}
+
+.airport p:last-child {
+  font-style: italic;
+  font-size: .8rem;
+}
+</style>
